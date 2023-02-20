48c2368dcc17cf0de3b6d6870f0ca9bacae778fc
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-20 10:53:48 +0100

    feat: move new site src & dist folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // TODO(HiDeoo) Rename later
+  srcDir: "./site-new",
+  // TODO(HiDeoo) Rename or remove later
+  outDir: "./dist-site-new",
+});
