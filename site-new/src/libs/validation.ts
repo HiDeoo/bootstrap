9c91f34c4b9d572b3db49923e36e2887a6afc489
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-24 14:15:22 +0100

    feat: refactor data access utility to be generic

--- site-new/src/libs/validation.ts
+++ site-new/src/libs/validation.ts
@@ -7,3 +7,12 @@ const semverRegex =
   /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
 
 export const zVersionSemver = z.string().regex(semverRegex)
+
+export const zHexColor = z.string().regex(/^#(?:[0-9a-fA-F]{3}){1,2}$/)
+
+export function zNamedHexColor<TName>(name: z.ZodLiteral<TName>) {
+  return z.object({
+    name,
+    hex: zHexColor,
+  })
+}
