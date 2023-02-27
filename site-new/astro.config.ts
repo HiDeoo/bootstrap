de2affdffb74955f82c7e225507d241c3d607550
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-27 12:19:39 +0100

    feat: shortcodes auto-import

--- site-new/astro.config.ts
+++ site-new/astro.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 
 import { bootstrap } from './src/libs/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [bootstrap(), mdx()],
+  integrations: [bootstrap()],
   markdown: {
     smartypants: false,
     syntaxHighlight: 'prism',
