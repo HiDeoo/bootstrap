71eb26e8056c235065dc913322d450f018d7a56c
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-27 09:47:36 +0100

    feat: add param replacement

--- site-new/astro.config.ts
+++ site-new/astro.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
+import { remarkBsParam } from './src/libs/remark'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
+    remarkPlugins: [remarkBsParam],
     syntaxHighlight: 'prism',
   },
   site: 'https://getbootstrap.com/',
