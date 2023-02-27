67dff0c12a87624f4211030513ec6858b4f34bad
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-27 09:49:50 +0100

    refactor: bstable

--- site-new/astro.config.ts
+++ site-new/astro.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
+import { rehypeBsTable } from './src/libs/rehype'
 import { remarkBsParam } from './src/libs/remark'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
+    rehypePlugins: [rehypeBsTable],
     remarkPlugins: [remarkBsParam],
     syntaxHighlight: 'prism',
   },
