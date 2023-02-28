dfd85de595bdc14abf99bbef980ede9262cff2f2
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-28 09:24:57 +0100

    feat: auto import all callouts and shortcodes

--- site-new/src/libs/astro.ts
+++ site-new/src/libs/astro.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import path from 'node:path'
 import mdx from '@astrojs/mdx'
 import type { AstroIntegration } from 'astro'
@@ -9,22 +10,11 @@ import { remarkBsParam, remarkBsDocsref } from './remark'
 // The docs directory path relative to the root of the project.
 const docsDirectory = './site-new'
 
-// A list of components that should be auto imported in all pages for convenience.
-// Note: updating this list requires a restart of the dev server.
-const autoImportedComponents = [
-  'src/components/GuideFooter.mdx',
-  'src/components/JsDataAttributes.mdx',
-  'src/components/callouts/InfoNpmStarter.mdx',
-  'src/components/shortcodes/AddedIn.astro',
-  'src/components/shortcodes/BsTable.astro',
-  'src/components/shortcodes/Callout.astro',
-  'src/components/shortcodes/CalloutDeprecatedDarkVariants.astro',
-  'src/components/shortcodes/Code.astro',
-  'src/components/shortcodes/DeprecatedIn.astro',
-  'src/components/shortcodes/JsDismiss.astro',
-  'src/components/shortcodes/Placeholder.astro',
-  'src/components/shortcodes/Table.astro',
-]
+// A list of directories in `src/components` that contains components that will be auto imported in all pages for
+// convenience.
+// Note: add a new directory to this list or a new component to one of the existing directories requires a restart of
+// the dev server.
+const autoImportedComponentDirectories = ['callouts', 'shortcodes']
 
 export function bootstrap(): AstroIntegration[] {
   return [
@@ -47,7 +37,26 @@ export function bootstrap(): AstroIntegration[] {
 }
 
 function bootstrap_auto_import() {
+  const autoImportedComponents: string[] = []
+
+  for (const autoImportedComponentDirectory of autoImportedComponentDirectories) {
+    const components = fs.readdirSync(
+      path.posix.join(process.cwd(), docsDirectory, 'src/components', autoImportedComponentDirectory),
+      {
+        withFileTypes: true,
+      }
+    )
+
+    for (const component of components) {
+      if (component.isFile()) {
+        autoImportedComponents.push(
+          `./${path.posix.join(docsDirectory, 'src/components', autoImportedComponentDirectory, component.name)}`
+        )
+      }
+    }
+  }
+
   return autoImport({
-    imports: autoImportedComponents.map((componentPath) => `./${path.posix.join(docsDirectory, componentPath)}`),
+    imports: autoImportedComponents,
   })
 }
