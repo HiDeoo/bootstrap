96eeaf5cfd0ba0fef2b642056e2e008928081b9c
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-03-05 12:21:01 +0100

    feat: docs sections links

--- site-new/src/libs/utils.ts
+++ site-new/src/libs/utils.ts
@@ -14,12 +14,13 @@ export function getSequence(start: number, end: number, step = 1) {
   return sequence
 }
 
-// This function is used in the docs sidebar to generate partial slugs and properly order the sidebar entries.
+// This function is used in the docs sidebar to generate partial slugs and properly order the sidebar entries and also
+// to generate docs frontmatter sections slugs.
 // Note: this should be refactored and removed, the sidebar ordering defined in `site/data/sidebar.yml` should not rely
 // on slugified custom titles that are expected to generate a string matching the actual file names on disk, this is
-// error prone. Instead, custom sidebar titles should be defined in the frtonmatter of the MDX files when needed and
+// error prone. Instead, custom sidebar titles should be defined in the frontmatter of the MDX files when needed and
 // `site/data/sidebar.yml` should only reference the actual file names and slug extracted from the docs content
-// collection.
+// collection. Same goes for the docs frontmatter sections.
 export function getSlug(str: string) {
   return slug(str).replace(/--+/g, '-')
 }
