ee8fc1f70ff5c3b261b5ae0368f0ad00407260f5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-03-09 17:34:15 +0100

    build: enable netlify preview url

--- site-new/astro.config.ts
+++ site-new/astro.config.ts
@@ -5,15 +5,12 @@ import { getConfig } from './src/libs/config'
 
 const isDev = process.env.NODE_ENV === 'development'
 
-// TODO(HiDeoo) Replace occurences of `DEPLOY_URL` with `DEPLOY_PRIME_URL` to use preview URLs.
 const site = isDev
   ? // In development mode, use the local dev server.
     'http://localhost:3000'
-  : // : process.env.DEPLOY_PRIME_URL !== undefined
-  process.env.DEPLOY_URL !== undefined
+  : process.env.DEPLOY_PRIME_URL !== undefined
   ? // If deploying on Netlify, use the `DEPLOY_PRIME_URL` environment variable.
-    // process.env.DEPLOY_PRIME_URL
-    process.env.DEPLOY_URL
+    process.env.DEPLOY_PRIME_URL
   : // Otherwise, use the `baseURL` value defined in the `config.yml` file.
     getConfig().baseURL
 
